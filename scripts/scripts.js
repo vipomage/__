9241583eb6a0055ad7740bd330634cb07bee4f5e
vipomage <vipomage@gmail.com>
2018-02-28 08:13:04 +0200

    contracts_005_&_0005_Upload

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,22 +3,25 @@ $(document).ready(function () {
     let settings = {
         "async": true,
         "crossDomain": true,
-        "url": "http://api-ropsten.etherscan.io/api?module=account&action=txlistinternal&address=0xE8ABF2f63c88cA71506DB5d099513C16990ce207&startblock=0&endblock=2702578&sort=asc&apikey=YourApiKeyTokenH37ZC3XHSE7FTRTQCVARKZWC4PFUHVKCB9",
+        "url":
+         "http://api.etherscan.io/api?module=account&action=txlistinternal&address=<CONTRACT_ADRESS>&startblock=0&endblock=2702578&sort=asc&apikey=YourApiKeyToken<API_KEY>",
         "method": "GET"
     };
     $.ajax(settings).done(function (response) {
-       /!* populateData(response,1);
-        populateData(response,0.5);
-        populateData(response,0.05);*!/
+        populateData(response, 1);
+        populateData(response, 0.5);
+        populateData(response, 0.05);
     });
 
-    function populateData(response,gameValue) {
+    function populateData(response, gameValue) {
         let game;
-        if (gameValue === 1){
+        if (gameValue === 1) {
             game = 'one';
-        }if(gameValue ===0.5){
+        }
+        if (gameValue === 0.5) {
             game = '05';
-        }if (gameValue === 0.05) {
+        }
+        if (gameValue === 0.05) {
             game = '005'
         }
         for (let i = 0; i < 10; i++) {
@@ -26,11 +29,12 @@ $(document).ready(function () {
             let toAddress = obj['to'];
             let value = obj['value'];
             if (value.substring(0, 3) / 100 !== 0.01) {
-                $(`.${game}Eth-Players`).append(`<li class="address"><a href="https://ropsten.etherscan.io/address/${toAddress}">${toAddress} ->
+                $(`.${game}Eth-Players`).append(`<li class="address"><a href="https://etherscan.io/address/${toAddress}">${toAddress} ->
  <span class="text-success">${value.substring(0, 3) / 100}</a></span></li>`)
             }
         }
     }
 
 });
+
 */
